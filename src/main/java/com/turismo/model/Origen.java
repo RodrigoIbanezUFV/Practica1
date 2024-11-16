db78f28f1e086f3a9dde7fd38f382c6d554fee97
= <=>
2024-11-16 12:25:46 +0100

    Codificacion corregida del modelaje de la comunidad y provincia de origen del registro

--- src/main/java/com/turismo/model/Origen.java
+++ src/main/java/com/turismo/model/Origen.java
@@ -1,5 +1,36 @@
 package com.turismo.model;
 
 public class Origen {
+    private String comunidad;
+    private String provincia;
 
+    public Origen(String comunidad, String provincia) {
+        this.comunidad = comunidad;
+        this.provincia = provincia;
+    }
+
+    // Getters y setters
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return "Origen{" +
+                "comunidad='" + comunidad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                '}';
+    }
 }
