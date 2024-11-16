ae15adcf2217476d94faa8fa6dc677f490d6ae33
= <=>
2024-11-16 12:29:56 +0100

    Codificacion modelaje de la comunidad y provincia de destino del registro.

--- src/main/java/com/turismo/model/Destino.java
+++ src/main/java/com/turismo/model/Destino.java
@@ -1,4 +1,36 @@
 package com.turismo.model;
 
 public class Destino {
+    private String comunidad;
+    private String provincia;
+
+    public Destino(String comunidad, String provincia) {
+        this.comunidad = comunidad;
+        this.provincia = provincia;
+    }
+
+    // Getters y setters
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return "Destino{" +
+                "comunidad='" + comunidad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                '}';
+    }
 }
