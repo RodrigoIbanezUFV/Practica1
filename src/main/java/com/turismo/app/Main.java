414870b45c536a768b08150f5572cc88c70ed7f7
= <=>
2024-11-16 13:09:48 +0100

    Avance hasta la codificacion del JSON con agrupado por comunidades.

--- src/main/java/com/turismo/app/Main.java
+++ src/main/java/com/turismo/app/Main.java
@@ -1,25 +1,25 @@
 package com.turismo.app;
 
 import com.turismo.model.TurismoRegistro;
+import com.turismo.servicios.Agrupador;
 import com.turismo.utils.LectorCSV;
-import com.turismo.utils.EscribirJSON;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Ruta del archivo CSV y nombre del archivo JSON de salida
+        // Rutas de archivos
         String rutaCSV = "src/main/resources/TurismoComunidades.csv";
-        String rutaJSON = "src/main/resources/TurismoComunidades.json";
+        String rutaJSONAgrupado = "src/main/resources/Comunidades_agrupadas.json";
 
-        // Crear instancias de utilidades
+        // Crear instancias de utilidades y servicios
         LectorCSV lector = new LectorCSV();
-        EscribirJSON escritor = new EscribirJSON();
+        Agrupador agrupador = new Agrupador();
 
         // Leer registros desde el archivo CSV
         List<TurismoRegistro> registros = lector.leeCSV(rutaCSV);
 
-        // Escribir registros en un archivo JSON
-        escritor.escribeJSON(registros, rutaJSON);
+        // Agrupar por comunidad de destino y escribir en JSON
+        agrupador.agruparPorComunidadDestino(registros, rutaJSONAgrupado);
     }
 }
